bf7eda9e455d0b1cab42176314d2667758b1fd90
Jashveragiwala <66709554+Jashveragiwala@users.noreply.github.com>
2024-12-10 19:52:17 +0800

    minor

--- node/chunker.go
+++ node/chunker.go
@@ -43,7 +43,7 @@ func (n *Node) Chunker(fileName string, targetNodeIP string, startTime time.Time
 
 	// Dynamically calculate chunk size
 	chunkSize = int(math.Ceil(float64(fileSize) / float64(numChunks)))
-	fmt.Printf("Chunk size: %v, number of Chunks: %v", chunkSize, numChunks)
+	// fmt.Printf("Chunk size: %v, number of Chunks: %v", chunkSize, numChunks)
 	// Open the source file
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -142,7 +142,7 @@ func (n *Node) Chunker(fileName string, targetNodeIP string, startTime time.Time
 	fmt.Printf("Going to send to chunk location receiver\n")
 	// fmt.Printf("Kill the target node in the 3 second duration.\n")
 	// time.Sleep(3 * time.Second)
-	
+
 	retryInterval := 2 * time.Second
 	retryStartTime := time.Now()
 	var sendErr error
@@ -335,9 +335,9 @@ func (n *Node) ChunkLocationReceiver(message Message, reply *Message) error {
 }
 
 func (n *Node) AssemblerComplete(message Message, reply *Message) error {
-	green := "\033[32m"  // ANSI code for red text
-	reset := "\033[0m" // ANSI code to reset color
+	green := "\033[32m" // ANSI code for red text
+	reset := "\033[0m"  // ANSI code to reset color
 	fmt.Printf("File Transfer has successfully completed.\n")
-	fmt.Printf(green + "Time taken: %v\n" + reset, time.Since(n.StartReq))
+	fmt.Printf(green+"Time taken: %v\n"+reset, time.Since(n.StartReq))
 	return nil
 }
